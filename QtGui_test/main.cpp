a53a6d5f2b85eaff34b24249dec5c4d35454c4b6
Hang Yuan <hangyuan@umich.edu>
2017-05-08 16:24:30 -0400

    mainwindow toy example

--- QtGui_test/main.cpp
+++ QtGui_test/main.cpp
@@ -1,10 +1,18 @@
-#include "QtGui_test.h"
 #include <QtWidgets/QApplication>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QSpinBox>
+
+#include "mainwindow.h"
+
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	QtGui_test w;
-	w.show();
-	return a.exec();
+	
+	MainWindow mainWin;
+	mainWin.show();
+
+	return a.exec(); //enter the event loop
 }
